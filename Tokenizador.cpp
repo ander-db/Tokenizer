57cc19f54037a840e3c74b6b195f97a9227f4bea
Ander Dorado <adb35@alu.ua.es>
2022-01-31 22:35:14 +0100

    Prototipos y metodos vacios

--- Tokenizador.cpp
+++ Tokenizador.cpp
@@ -15,6 +15,18 @@ Tokenizador::Tokenizador(const string &delimitadoresPalabra, const bool &kcasosE
 }
 
 /**
+ * @brief Constructor de copia
+ *
+ * @param tokenizador Tokenizador a copiar.
+ */
+Tokenizador::Tokenizador(const Tokenizador &tokenizador)
+{
+	this->delimiters = tokenizador.delimiters;
+	this->casosEspeciales = tokenizador.casosEspeciales;
+	this->pasarAminuscSinAcentos = tokenizador.pasarAminuscSinAcentos;
+}
+
+/**
  * @brief Constructor por defecto. Delimitadores por defecto, casosEspciales = true y pasarAminuscSinAcentos = false;
  *
  */
@@ -26,15 +38,21 @@ Tokenizador::Tokenizador()
 }
 
 /**
- * @brief Constructor de copia
- *
- * @param tokenizador Tokenizador a copiar.
+ * @brief Destructor de la clase Tokenizador. Establece delimiters a "".
+ * 
  */
-Tokenizador::Tokenizador(const Tokenizador &tokenizador)
+Tokenizador::~Tokenizador()
 {
-	this->delimiters = tokenizador.delimiters;
-	this->casosEspeciales = tokenizador.casosEspeciales;
-	this->pasarAminuscSinAcentos = tokenizador.pasarAminuscSinAcentos;
+}
+
+/**
+ * @brief Sobrecarga del operador copia.
+ * 
+ * @param tokenizador Tokenizador a copiar.
+ * @return Nuevo tokenizador.
+ */
+Tokenizador &Tokenizador::operator=(const Tokenizador &tokenizador)
+{
 }
 
 /**
@@ -71,7 +89,89 @@ bool Tokenizador::Tokenizar(const string &i) const
 {
 }
 
+/**
+ * @brief  Tokeniza el fichero i que contiene un nombre de fichero por línea guardando la salida en ficheros (uno por cada línea de i) cuyo nombre será el leído en i añadiéndole extensión .tk, y que contendrá una palabra en cada línea del fichero leído en i. Devolverá true si se realiza la tokenización de forma correcta de todos los archivos que contiene i; devolverá false en caso contrario enviando a cerr el mensaje correspondiente (p.ej. que no exista el archivo i, o que se trate de un directorio, enviando a “cerr” los archivos de i que no existan o que sean directorios; luego no se ha de interrumpir la ejecución si hay algún archivo en i que no exista)
+ * 
+ * @param i Fichero con lista de ficheros a tokenizar.
+ * @return true Se han podido tokenizar los archivos de i con exito.
+ * @return false Si no se ha podido tokenizar con exito. P.ej. No existen los directorios de i. 
+ */
+bool Tokenizador::TokenizarListaFicheros(const string &i) const
+{
+	return false;
+}
+
+/**
+ * @brief Tokeniza todos los archivos que contenga el directorio i, incluyendo los de los subdirectorios, guardando la salida en ficheros cuyo nombre será el de entrada añadiéndole extensión .tk, y que contendrá una palabra en cada línea del fichero. 
+ * 
+ * @param i Directorio
+ * @return true Se ha realizado la tokenizacion de forma correcta de los archivos del directorio i.
+ * @return false No se ha podido realizar la tokenizacion. P.ej. No existe el directorio i
+ */
 bool Tokenizador::TokenizarDirectorio(const string &i) const
 {
 	return false;
+}
+
+/**
+ * @brief Cambia “delimiters” por “nuevoDelimiters” comprobando que no hayan delimitadores repetidos (de izquierda a derecha), en cuyo caso se eliminarían los que hayan sido repetidos (los nuevos delimitadores que se van analizando) tanto en “nuevoDelimiters” como los que ya estuviesen en “delimiters” 
+ * 
+ * @param nuevoDelimiters Nuevos delimitadores
+ */
+void Tokenizador::DelimitadoresPalabra(const string &nuevoDelimiters)
+{
+}
+
+/**
+ * @brief Añade al final de “delimiters” los nuevos delimitadores que aparezcan en “nuevoDelimiters” (no se almacenarán caracteres repetidos)
+ * 
+ * @param nuevoDelimiters Delimiter a añadir.
+ */
+void Tokenizador::AnyadirDelimitadoresPalabra(const string &nuevoDelimiters)
+{
+}
+
+/**
+ * @brief Getter de la variable "delimiters".
+ * 
+ * @return Delimiters 
+ */
+string Tokenizador::DelimitadoresPalabra() const
+{
+	return this->delimiters;
+}
+
+/**
+ * @brief Setter de la variable casosEspeciales
+ * 
+ * @param nuevoCasosEspeciales Nuevo casos especiales
+ */
+void Tokenizador::CasosEspeciales(const bool &nuevoCasosEspeciales)
+{
+	this->casosEspeciales = nuevoCasosEspeciales;
+}
+
+/**
+ * @brief Getter de casos especiales 
+ */
+bool Tokenizador::CasosEspeciales()
+{
+	return this->casosEspeciales;
+}
+
+/**
+ * @brief Setter de la variable pasarAminuscSinAcentos 
+ * 
+ * @param nuevoPasarAminuscSinAcentos Nuevo valor de pasarAmunuscSinAcentos
+ */
+void Tokenizador::PasarAminuscSinAcentos(const bool &nuevoPasarAminuscSinAcentos)
+{
+}
+
+/**
+ * @brief Getter de pasarAminuscSinAcentos
+ */
+bool Tokenizador::PasarAminuscSinAcentos()
+{
+	return this->pasarAminuscSinAcentos;
 }
